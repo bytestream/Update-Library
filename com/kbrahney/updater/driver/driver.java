a15d6f126ca7210661108e6e0a8017f28245a634
Chris Foxley-Evans <cfoxleyevans.compsci@gmail.com>
2013-05-23 23:13:18 +0100

    New hand coded gui in com.kbrhaney.updater.GUI changed pakcages to have uppercase names new driver package that has a main method

---
+++ com/kbrahney/updater/driver/driver.java
@@ -0,0 +1,20 @@
+package com.kbrahney.updater.Driver;
+
+
+import com.kbrahney.updater.GUI.GUIInterface;
+import com.kbrahney.updater.GUI.Interface;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chris
+ * Date: 23/05/2013
+ * Time: 21:49
+ * To change this template use File | Settings | File Templates.
+ */
+public class Driver {
+
+    public static void main (String[] args) {
+        Interface g = new Interface();
+        GUIInterface g2 = new GUIInterface();
+    }
+}
